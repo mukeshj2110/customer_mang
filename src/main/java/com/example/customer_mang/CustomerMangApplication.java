7b511861d6e1fbaecbe99a428e0b5765d29b3268
mukeshj2110 <mukeshjadhav680@gmail.com>
2021-12-27 23:47:21 +0530

    initial commit

---
+++ src/main/java/com/example/customer_mang/CustomerMangApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_mang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customer_mang/CustomerMangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_mang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
